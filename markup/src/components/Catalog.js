e9de5bc74706021a48a7f366da6ec3ec66f78efb
Andrey Ivanov <immxoff@mail.ru>
2018-04-24 21:26:17 +0300

    подъехал сервер и монга

---
+++ markup/src/components/Catalog.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import styles from '../styles/Catalog.module.css'
+
+class Catalog extends Component {
+    render() {
+        return <div className={styles.catalog}>
+                    <a className={styles.catalog__link} href="/users">Кухни</a>
+                    <a className={styles.catalog__link} href="">Спальни</a>
+                    <a className={styles.catalog__link} href="">Подъемные кровати</a>
+                    <a className={styles.catalog__link} href="">Гостиные</a>
+                    <a className={styles.catalog__link} href="">Шкафы</a>
+                    <a className={styles.catalog__link} href="">Гардеробные</a>
+                    <a className={styles.catalog__link} href="">Прихожие</a>
+                    <a className={styles.catalog__link} href="">Детская мебель</a>
+                    <a className={styles.catalog__link} href="">Торговые острова</a>
+                    <a className={styles.catalog__link} href="">Еще <i className="fas fa-caret-down"></i></a>
+                </div>
+    }
+}
+
+export default Catalog
---
+++ routes/orders.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const db = require('../dbconfig/db');
+
+mongoose.connect(db.url);
+
+const Schema = mongoose.Schema;
+const OrderSchema = new Schema({
+    name     : String,
+    tel      : Number,
+    email    : String,
+    comment  : String
+});
+const Order = mongoose.model("Order", OrderSchema);
+
+router.get('/', function(req, res) {
+    Order.find({}, function(err, data){
+        if(err) return console.log(err);
+        res.send(data);
+    });
+
+});
+
+router.post('/insert', function(req, res) {
+    const order = new Order({
+        name: req.body.name,
+        tel: req.body.tel,
+        email: req.body.email,
+        comment: req.body.comment
+    });
+    order.save(function (err) {
+        if(err) console.log(err);
+        res.send(order);
+    });
+
+});
+
+
+module.exports = router;
